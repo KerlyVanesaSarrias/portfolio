61a86a9f56aca1cb612bb3b5248fa6d3d955e4c4
Kerly Vanessa Sarrias <kerlysarrias011@gmail.com>
2024-04-25 15:10:05 -0500

    complete home page

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const Button = ({ color, size, children, onClick, isOnlyIcon = false }) => {
     const buttonClasses = classNames( {
-        'bg-cyan-500 text-white': color === 'primary',
-        'bg-red-500 text-white': color === 'secondary',
+        'bg-cyan-500 text-white rounded': color === 'primary',
+        'bg-gray-500 text-white': color === 'secondary',
         'w-32 h-12 px-4 py-2': size === 'small',
         'w-48 h-12 px-4 py-2': size === 'medium',
-        '!rounded-full h-12 w-12  flex justify-center items-center border-2 border-white': isOnlyIcon
+        '!rounded-full h-12 w-12 text-xl flex justify-center items-center border-2 border-white': isOnlyIcon
     });
 
     return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,32 @@
 
 import Button from "../components/Button/Button"
-import 'boxicons'
+import { FaGithub, FaLinkedin, FaWhatsapp} from "react-icons/fa";
+import'./Styles/HomePage.css'
+
 
 
 const HomePage = () => {
     return (
-        <div className='flex flex-row items-center bg-[#07233B]'>
-            <div className="w-1/2">
+        <div className='flex flex-row items-center bg-[#07233B] px-8'>
+            <div className="w-3/5">
                 <h1 className="bg-[#00334E] uppercase text-white p-1 rounded font-normal tracking-widest w-36">Kerly Sarrias</h1>
-                <h2 className=" text-white font-bold uppercase text-6xl">Hay! i am Kerly Vanessa Sarrias</h2>
-                <div>
+                <h2 className=" text-white font-bold uppercase text-6xl ">Hay! i am Kerly Vanessa Sarrias</h2>
+                <div className="flex gap-3">
                     <h3 className=" text-cyan-500 font-bold uppercase text-6xl">I'm a</h3>
-                    <h4 className=" text-cyan-500 font-bold uppercase text-6xl">developer</h4>
+                    <h4 className="text-cyan-500 font-bold uppercase text-6xl home_animation">developer</h4>
                 </div>
-                <p className="text-white font-normal text-xl leading-normal tracking-wider" >Full Stack Web Developer, passionate about creating memorable and functional digital experiences. With skills in multiple technologies and a focus on development efficiency.</p>
-                <div className="flex gap-3 ">
-                    <Button color="primary" size="small" isRounded>Descargar CV</Button>
-                    <Button isOnlyIcon='icon' color='secondary'  ><box-icon type='logo' name='github'></box-icon></Button>
-                    <Button isOnlyIcon='icon' color='secondary'  ><box-icon type='logo' name='linkedin'></box-icon></Button>
-                    <Button isOnlyIcon='icon' color='secondary'  ><box-icon type='logo' name='whatsapp'></box-icon></Button>
+                <p className="text-white font-normal text-xl leading-normal tracking-wider mt-4 " >Full Stack Web Developer, passionate about creating memorable and functional digital experiences. With skills in multiple technologies and a focus on development efficiency.</p>
+                <div className="flex gap-3 mt-6">
+                    <Button color="primary" size="small" isRounded>Downland CV</Button>
+                    <Button isOnlyIcon='icon' color='secondary'  ><FaGithub/></Button>
+                    <Button isOnlyIcon='icon' color='secondary'  ><FaLinkedin/></Button>
+                    <Button isOnlyIcon='icon' color='secondary'  ><FaWhatsapp/></Button>
                 </div>
             </div>
-            <div ><img className="" src="/src/assets/logo3.png" alt="" /></div>
+            <div>
+            <img className="" src="/src/assets/logo3.png" alt="" />
+            </div>
+    
         </div>
     )
 }

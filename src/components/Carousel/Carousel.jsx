ef237cb1566442a16a028d32bdd7ea0ff004a3b9
Kerly Vanessa Sarrias <kerlysarrias011@gmail.com>
2024-06-20 08:18:11 -0500

    img projects

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -10,21 +10,21 @@ const Carousel = () => {
     const projects = [
         {
             id: 1,
-            image: {Fortune},
+            image: [Fortune],
             link: 'https://fortune-cookie-omega.vercel.app/',
             title: 'Fortune Cookie',
             description: 'A web app that generates random fortune cookie messages'
         },
         {
             id: 2,
-            image: {Weather},
+            image: [Weather],
             link: 'https://weather-app-pi-ten-18.vercel.app/',
             title: 'Weather App',
             description: 'A web app that displays current weather and forecast for a given location'
         },
         {
             id: 3,
-            image: {User},
+            image: [User],
             link: 'https://users-crud-three.vercel.app/',
             title: 'Users CRUD',
             description: 'A web app that allows users to create, read, update and delete user data',
@@ -32,14 +32,14 @@ const Carousel = () => {
         },
         {
             id: 4,
-            image: {RickAndMorty},
+            image: [RickAndMorty],
             link: 'https://rick-and-morty-app-pi-umber.vercel.app/',
             title: 'Rick and Morty App',
             description: 'A web app that displays characters and episodes from the Rick and Morty TV series'
         },
         {
             id: 5,
-            image: {Pokedex},
+            image: [Pokedex],
             link: 'https://pokedex-omega-khaki.vercel.app/',
             title: 'Pokedex',
             description: 'A web app that displays information about Pokémons'

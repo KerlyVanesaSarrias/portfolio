2ef8f206ea6aa3d6d58e00174bc13ee72544b643
Kerly Vanessa Sarrias <kerlysarrias011@gmail.com>
2024-07-10 17:45:21 -0500

    ...

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,10 +5,10 @@ import InfContact from '../components/Contact/InfContact'
 const ContactPage = () => {
     return (
         <div className='w-full m-0'>
-            <h1 className='pl-10 pr-10'> <div className='brand mt-7'>CONTACT ME </div></h1>
+            <h1 className='pl-10 pr-10'> <div className='brand pt-7'>CONTACT ME </div></h1>
             <div className='w-full flex flex-col md:flex-row  justify-center'>
                 <div className='w-full'><FormContact/></div> 
-                <div className=' text-lg md:text-xl mt-8 md:mt-16 mb-10 w-1/2'><InfContact/></div>
+                <div className=' text-lg md:text-xl  w-1/2'><InfContact/></div>
             </div>
         </div>
     )

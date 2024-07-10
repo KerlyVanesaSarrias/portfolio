74ee813c8d3286475c4fe0d8c6ea3d132ec2ce5e
Kerly Vanessa Sarrias <kerlysarrias011@gmail.com>
2024-07-10 17:42:13 -0500

    inf contact

--- src/components/Contact/InfContact.jsx
+++ src/components/Contact/InfContact.jsx
@@ -5,7 +5,7 @@ import { MdOutlinePhoneAndroid, MdOutlineMail, MdOutlineLocationOn   } from "rea
 
 const InfContact = () => {
     return (
-    <div className='max-w-md ml-20' >
+    <div className='max-w-md pl-20' >
         <div className=' dark-text gap-10 w-96'>
             <h3 className='brand inline-block  text-base'>CONTACT INFORMATION</h3>
             <address>

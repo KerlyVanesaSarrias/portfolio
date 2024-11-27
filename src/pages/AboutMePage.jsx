e01c7e3985bb60a5a319af2240c36d9f14441820
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-11-27 11:19:20 -0500

    center button

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -33,13 +33,13 @@ const education = t("education", { returnObjects: true });
             <img className=" rounded-3xl" src={LogoImg} alt="" />
           </div>
         </div>
-        <div className=' order-1 md:order-2 flex flex-col gap-1 md:w-3/5 md:pr-24 w-full '>
+        <div className=' order-1 md:order-2 flex flex-col justify-center items-center gap-1 md:w-3/5 md:pr-24 w-full '>
           <div className='absolute md:top-28 top-30 right-1   -z-30 '><img src={effect2} alt="Logo" /></div>
           <h1 className="brand w-32 ">{t("about.title")}</h1>
           <h2 className="  dark-text mt-6 font-bold uppercase text-4xl 2xl:text-7xl  ">{t("about.head1")}</h2>
           <h3 className="  dark-text  font-bold uppercase text-[#008DDA] text-4xl 2xl:text-7xl  ">{t("about.head2")} </h3>
           <p className=" dark-text  text-lg  2xl:text-3xl mt-7 text-justify  ">{t("about.description")}</p>
-          <Button onClick={handleDownload} className="md:mt-10 " color="primary" size="small" isRounded>{t("about.downloadCV")}</Button>
+          <Button onClick={handleDownload} className="md:mt-10  mt-5" color="primary" size="small" isRounded>{t("about.downloadCV")}</Button>
         </div>
       </div>
 
